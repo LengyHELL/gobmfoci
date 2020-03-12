e7c5905db6c18ed5006d38e78ee1878d7d070fd7
LengyHELL <ubi19998@gmail.com>
2020-03-12 11:39:43 +0100

    uj iranyitas, apro valtoztatas a fizikai szamolasban

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,15 @@
 var board = {
   canvas : document.getElementById("cv1"),
-  mousePos : { x : 1, y : 0, but : 0 },
+  mousePos : { x : 1, y : 0, but : 0 , whl : 0},
   clickLock : true,
+  update : 50,
   set : function() {
     this.context = this.canvas.getContext("2d");
     this.clickLock = true;
     this.canvas.addEventListener("mousemove", getMousePos, false);
     this.canvas.addEventListener("mousedown", clickStart, false);
     this.canvas.addEventListener("mouseup", clickEnd, false);
+    this.canvas.addEventListener("wheel", onWheel, false);
     this.canvas.addEventListener("mousedown", function(evt) { evt.preventDefault(); }, false);
     this.canvas.addEventListener("contextmenu", function(evt) { evt.preventDefault(); }, false);
   },
@@ -19,14 +21,16 @@ var board = {
 
 var game = {
   balls : [],
+  selected : -1,
+  power : 1000,
   //ballPic : new Image(0, 0),
-  set : function() {
+  set : function(p) {
     //this.ballPic.src = "/gombfoci/scripts/ball.png?" + new Date().getTime();
     this.frames = 0;
-    this.balls.push(new Ball(50, 50, 15, 5));
-    this.balls.push(new Ball(100, 100, 15, 5));
-    this.balls.push(new Ball(200, 200, 15, 5));
-  }
+    this.balls.push(new Ball(50, 50, 10, 4, 0));
+    this.balls.push(new Ball(100, 100, 15, 6, 1));
+    this.balls.push(new Ball(200, 200, 15, 6, 2));
+  },
 };
 
 
@@ -52,11 +56,12 @@ function getdeg(vec1, vec2) {
 
 
 
-function Ball(x, y, r, m) {
+function Ball(x, y, r, m, t) {
   this.pos = new Vector(x, y);
   this.force = new Vector(0, 0);
   this.radius = r;
   this.mass = m;
+  this.type = t;
 }
 
 function testcol(ball1, ball2) {
@@ -82,28 +87,58 @@ function start() {
   game.set();
   board.resize(640, 480);
   clearInterval(interval);
-  interval = setInterval(update, 1000 / 50);
+  interval = setInterval(draw, 1000 / board.update)
+  interval = setInterval(update, 1);
 }
 
 function end() {
   clearInterval(interval);
 }
 
+function ismoving() {
+  temp = 0;
+  limit = 0.5;
+  for (var i = 0; i < game.balls.length; i++) {
+    temp += magn(game.balls[i].force);
+  }
+  if (temp > limit) { return true; }
+  else { return false; }
+}
+
 function update() {
-  resist = 0.90;
-  power = 0.1;
+  resist = 0.01;
+
+  game.power += board.mousePos.whl * 1000;
+  if (game.power < 0) { game.power = 0; }
+  if (game.power > 15000) { game.power = 15000; }
+  board.mousePos.whl = 0;
+  document.getElementById("power").innerHTML = game.power;
 
   if (board.mousePos.but == 0) { board.clickLock = false; }
-  else if ((board.mousePos.but > 0) && !board.clickLock) {
+  else if ((board.mousePos.but > 0) && !board.clickLock && !ismoving()) {
     board.clickLock = true;
 
-    if (board.mousePos.but == 1) {
-      game.balls[0].force.x += (board.mousePos.x - game.balls[0].pos.x) * power;
-      game.balls[0].force.y += (board.mousePos.y - game.balls[0].pos.y) * power;
+    if ((board.mousePos.but == 1) && (game.selected >= 0)) {
+      dir = new Vector(board.mousePos.x - game.balls[game.selected].pos.x, board.mousePos.y - game.balls[game.selected].pos.y)
+      m = magn(dir);
+      dir.x /= m;
+      dir.y /= m;
+      game.balls[game.selected].force.x += dir.x * game.power;
+      game.balls[game.selected].force.y += dir.y * game.power;
+      game.selected = -1;
     }
     if (board.mousePos.but == 2) {
-      game.balls[1].force.x += (board.mousePos.x - game.balls[1].pos.x) * power;
-      game.balls[1].force.y += (board.mousePos.y - game.balls[1].pos.y) * power;
+      sel = false;
+      for (var i = 0; (i < game.balls.length) && !sel; i++) {
+        dv = new Vector(game.balls[i].pos.x - board.mousePos.x, game.balls[i].pos.y - board.mousePos.y);
+        if ((magn(dv) < (game.balls[i].radius)) && (game.balls[i].type != 0)) {
+          sel = true;
+          game.selected = i;
+        }
+        else {
+          game.selected = -1;
+        }
+      }
     }
   }
 
@@ -140,10 +175,10 @@ function update() {
       }
     }
 
-    game.balls[i].pos.x += game.balls[i].force.x;
-    game.balls[i].pos.y += game.balls[i].force.y;
-    game.balls[i].force.x *= resist;
-    game.balls[i].force.y *= resist;
+    game.balls[i].pos.x += game.balls[i].force.x / 1000;
+    game.balls[i].pos.y += game.balls[i].force.y / 1000;
+    game.balls[i].force.x -= game.balls[i].force.x * resist;
+    game.balls[i].force.y -= game.balls[i].force.y * resist;
   }
 
   draw();
@@ -153,12 +188,31 @@ function draw() {
   board.context.clearRect(0, 0, board.canvas.width, board.canvas.height);
   ctx = board.context;
   for (var i = 0; i < game.balls.length; i++) {
-    if (i == 0) { ctx.strokeStyle = "red"; }
-    else if (i == 1) { ctx.strokeStyle = "blue"; }
-    else { ctx.strokeStyle = "black"; }
     ctx.beginPath();
     ctx.arc(game.balls[i].pos.x, game.balls[i].pos.y, game.balls[i].radius, 0, 2 * Math.PI);
+
+    if (game.balls[i].type == 1) {
+      ctx.strokeStyle = "red";
+      ctx.fillStyle = "red";
+    }
+    else if (game.balls[i].type == 2) {
+      ctx.strokeStyle = "blue";
+      ctx.fillStyle = "blue";
+    }
+    else {
+      ctx.strokeStyle = "black";
+      ctx.fillStyle = "black";
+    }
+
+    ctx.fill();
     ctx.stroke();
+
+    if (i == game.selected) {
+      ctx.beginPath();
+      ctx.arc(game.balls[i].pos.x, game.balls[i].pos.y, game.balls[i].radius + 10, 0, 2 * Math.PI);
+      ctx.strokeStyle = "black";
+      ctx.stroke();
+    }
     //ctx.drawImage(game.ballPic, game.balls[i].pos.x - (31 / 2), game.balls[i].pos.y - (31 / 2), 31, 31);
   }
 }
@@ -175,4 +229,14 @@ function clickStart(evt) {
 
 function clickEnd(evt) {
   board.mousePos.but = 0;
+}
+
+function onWheel(evt) {
+  evt.preventDefault();
+  if (evt.deltaY > 0) {
+    board.mousePos.whl = -1;
+  }
+  else if (evt.deltaY < 0) {
+    board.mousePos.whl = 1;
+  }
 }
